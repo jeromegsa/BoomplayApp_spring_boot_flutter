5e8508f8f3dea59b6c8dff27afe2b91f9985945d
jeromegsa <jeromegbossa3@gmail.com>
2024-10-22 12:00:53 +0100

    initialisation

---
+++ boomplay/src/main/java/com/winner/boomplay/BoomplayApplication.java
@@ -0,0 +1,13 @@
+package com.winner.boomplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomplayApplication.class, args);
+	}
+
+}
---
+++ boomplay/src/test/java/com/winner/boomplay/BoomplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winner.boomplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoomplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
