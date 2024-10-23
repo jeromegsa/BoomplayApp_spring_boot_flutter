447bf522bb813c2a938584963e69fc7bf6191000
arnaudsonkpianphp <arnaudsonkpian205@gmail.com>
2024-10-23 10:57:24 +0100

    le connectionPage est oklm!

--- frontend/lib/Screens/home_page.dart
+++ frontend/lib/Screens/home_page.dart
@@ -3,22 +3,54 @@ import 'package:frontend/Screens/delayedAnimation.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/casque.png'),
-            fit: BoxFit.cover,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(190),
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(20), 
+              top: Radius.circular(5),
+            ),
+          ),
+          child: AppBar(
+            title: const Center(
+              child: Text(
+                'BOOM PLAY',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            toolbarHeight: 190,
+            backgroundColor: Colors.transparent, 
+            flexibleSpace: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  'Votre plateforme de musique préférée, connectez-vous et amusez-vous!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MyForm(),
-          ],
-        ),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MyForm(),
+        ],
       ),
     );
   }
@@ -73,94 +105,109 @@ class MyFormState extends State<MyForm> {
             padding: const EdgeInsets.all(16.0),
             child: Center(
               child: SizedBox(
-                child: SizedBox(
-                  width: 500,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const DelayedAnimation(
-                          delay:10000,
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
+                width: 500,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const DelayedAnimation(
+                        delay: 5000,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.redAccent,
                           ),
                         ),
-                        const SizedBox(height: 20),
-                        DelayedAnimation(
-                          delay: 500,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextFormField(
-                              controller: emailController,
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                              
-                                prefixIcon: const Icon(Icons.mail),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
+                      ),
+                      const SizedBox(height: 20),
+                      DelayedAnimation(
+                        delay: 500,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              prefixIcon: const Icon(Icons.mail, color: Colors.redAccent),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: const BorderSide(color: Colors.redAccent, width: 2),
                               ),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Veuillez entrer un texte';
-                                }
-                                return null;
-                              },
-                            ),
-                          ),
-                        ),
-                        DelayedAnimation(
-                          delay: 5000,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextFormField(
-                             
-                              controller: passwordController,
-                              decoration: InputDecoration(
-                                labelText: 'Password',
-                                 prefixIcon:Icon(Icons.lock),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: const BorderSide(color: Colors.redAccent, width: 2),
                               ),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Veuillez entrer un texte';
-                                }
-                                return null;
-                              },
-                              obscureText: true,
+                              filled: true,
+                              fillColor: Colors.white,
                             ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        DelayedAnimation(
-                          delay: 8000,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(2, 168, 14, 245),
-                            ),
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text('Traitement des données')),
-                                );
-                                Verif(context);
+                            style: const TextStyle(color: Colors.black),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Veuillez entrer un texte';
                               }
+                              return null;
                             },
-                            child: const Text('Soumettre'),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      DelayedAnimation(
+                        delay: 3000,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              prefixIcon: const Icon(Icons.lock, color: Colors.redAccent),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: const BorderSide(color: Colors.redAccent, width: 2),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: const BorderSide(color: Colors.redAccent, width: 2),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            style: const TextStyle(color: Colors.black),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Veuillez entrer un texte';
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 70),
+                      DelayedAnimation(
+                        delay: 2000,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.redAccent,
+                            minimumSize: const Size(300, 50),
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Traitement des données')),
+                              );
+                              Verif(context);
+                            }
+                          },
+                          child: const Text(
+                            'Soumettre',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
