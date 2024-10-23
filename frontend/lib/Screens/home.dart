78c67f65ea108f527f96ce6a222c9b6313338be8
jeromegsa <jeromegbossa3@gmail.com>
2024-10-23 12:23:25 +0100

    mise en place du routage

--- frontend/lib/Screens/home.dart
+++ frontend/lib/Screens/home.dart
@@ -1,96 +1,120 @@
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-class home extends StatelessWidget {
-  const home({
+class Home extends StatelessWidget {
+  const Home({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Background Image
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/bg.jpg'),
-              fit: BoxFit.cover,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Boomplay Home'),
+        backgroundColor: const Color(0xffff735c),
+      ),
+      body: Stack(
+        children: [
+          // Image de fond
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/bg.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        // Dark Overlay for the Image
-        Container(
-          color: Colors.black.withOpacity(0.6), // Dark overlay
-        ),
-        // Content Overlay
-        Positioned(
-          right: 300, // Positioned to the right of the screen
-          top: 300,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // The welcome text split into two lines
-              const Text(
-                'Bienvenue sur',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 70,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                'Boomplay',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 70,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your play music functionality here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor:const Color(0xffff735c),
-                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                child: const Text(
-                  'Play Music',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 15),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your play video functionality here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
-                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                child: const Text(
-                  'Play Video',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ],
+          // Overlay sombre
+          Container(
+            color: Colors.black.withOpacity(0.7), // Overlay sombre
           ),
-        ),
-      ],
+          // Contenu centré
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Centrer verticalement
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Texte de bienvenue en deux lignes
+                const Text(
+                  'Bienvenue sur',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Boomplay',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ajouter votre fonctionnalité de lecture de musique ici
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffff735c),
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Augmenter le padding
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child:const  Row(
+                    mainAxisSize: MainAxisSize.min, // Pour que le bouton s'adapte au contenu
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:  [
+                      Icon(Icons.play_arrow, color: Colors.white), // Icône de lecture
+                      SizedBox(width: 8), // Espace entre l'icône et le texte
+                      Text(
+                        'Play Music',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ajouter votre fonctionnalité de lecture de vidéo ici
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Augmenter le padding
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min, // Pour que le bouton s'adapte au contenu
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.play_arrow, color: Colors.white), // Icône de lecture
+                      SizedBox(width: 8), // Espace entre l'icône et le texte
+                      Text(
+                        'Play Video',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,13 @@
-import "package:frontend/Screens/home.dart";
 import 'package:flutter/material.dart';
+
+import 'Screens/form_page.dart';
+import 'Screens/home.dart';
+// import 'Screens/signup.dart';
+// import 'Screens/music_list.dart';
+// import 'Screens/video_list.dart';
+// import 'Screens/play_music.dart';
+// import 'Screens/play_video.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,9 +17,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Boomplay ',
-      home: HomePage(),
+    return MaterialApp(
+      title: 'Boomplay',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/login': (context) => const MyForm(),
+        // '/signup': (context) => const SignupPage(),
+        // '/musics': (context) => const MusicListPage(),
+        // '/videos': (context) => const VideoListPage(),
+        // '/play-music': (context) => const PlayMusicPage(),
+        // '/play-video': (context) => const PlayVideoPage(),
+      },
     );
   }
 }
@@ -21,13 +41,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xffff735c)
+        title: const Text('Boomplay Home'),
+        backgroundColor: const Color(0xffff735c),
       ),
-      body: const home(),
     );
-  }
 }
-
-
+}
